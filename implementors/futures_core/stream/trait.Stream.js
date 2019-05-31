154a2a82b9726216d7ccb75d5d0e9eafbea002a6
Travis CI User <travis@example.org>
2019-05-31 11:38:06 +0000

    Documentation for rustasync/futures-timer@239d481

---
+++ implementors/futures_core/stream/trait.Stream.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["futures_timer"] = [{text:"impl <a class=\"trait\" href=\"https://rust-lang-nursery.github.io/futures-api-docs/0.3.0-alpha.16/futures_core/futures_core/stream/trait.Stream.html\" title=\"trait futures_core::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"futures_timer/struct.Interval.html\" title=\"struct futures_timer::Interval\">Interval</a>",synthetic:false,types:["futures_timer::interval::Interval"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["futures_timer"] = {"name":"","files":["arc_list.rs","delay.rs","ext.rs","global.rs","heap.rs","interval.rs","lib.rs"]};
+createSourceSidebar();
